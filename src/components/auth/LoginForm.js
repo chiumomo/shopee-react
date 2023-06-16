1cc50b8057a3e7740c9772a0a2babc0427e2e4e3
Momo Chiu <q10110144kh@gmail.com>
2023-06-17 03:03:33 +0800

    adjust

--- src/components/auth/LoginForm.js
+++ src/components/auth/LoginForm.js
@@ -5,26 +5,26 @@ import { useNavigate } from "react-router-dom";
 import AuthContext from "./AuthContext";
 
 const LoginForm = ()=>{
-    const Navigate = useNavigate();
-    const {login,isAuthenticated} = useContext(AuthContext);
-    const [username,setUsername] = useState();
-    const [password,setPassword] = useState();
+    // const Navigate = useNavigate();
+    // const {login,isAuthenticated} = useContext(AuthContext);
+    // const [username,setUsername] = useState();
+    // const [password,setPassword] = useState();
     const handleLogin =() =>{
         //call login api
-        console.log(username,password);
-        console.log(isAuthenticated);
+        // console.log(username,password);
+        // console.log(isAuthenticated);
         
-        login(username,password).then((token,error)=>{
-            if(!token){
-                message.error(error)
-            }
-        });
+        // login(username,password).then((token,error)=>{
+        //     if(!token){
+        //         message.error(error)
+        //     }
+        // });
         
     };
 
-    useEffect(()=>{
-        isAuthenticated && Navigate.push('/')
-    },[isAuthenticated])
+    // useEffect(()=>{
+    //     isAuthenticated && Navigate.push('/')
+    // },[isAuthenticated])
 
     return(
         <div>
@@ -33,8 +33,8 @@ const LoginForm = ()=>{
                 size="large" 
                 placeholder="帳號" 
                 prefix={<UserOutlined />} 
-                value={username}
-                onChange={e=> setUsername(e.target.value)}
+                // value={username}
+                // onChange={e=> setUsername(e.target.value)}
             />
             <Input 
                 className="mb-3"
@@ -42,8 +42,8 @@ const LoginForm = ()=>{
                 size="large" 
                 placeholder="密碼" 
                 prefix={<LockOutlined />} 
-                value={password}
-                onChange={e=> setPassword(e.target.value)}
+                // value={password}
+                // onChange={e=> setPassword(e.target.value)}
             />
             <Button onClick={handleLogin} >登入</Button>
         </div>
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -71,6 +71,7 @@ const Header =({className})=>{
                     <StyledToolBar>
                         <a href="#!" >通知</a>
                         <a href="#!" >幫助中心</a>
+                        <Link to="/shopee-react/login" >登入/註冊</Link>
                         {/* {isAuthenticated ?(
                             <div>
                                 <a href="#!" >user</a>
